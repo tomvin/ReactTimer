1448900ba16d6a2fe4eb3a553c92d0bce6e84fe3
Tom Vinnicombe <tom_vinnicombe@live.com.au>
2017-05-16 21:23:09 +1000

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <div className="top-bar">
+                    <div className="top-bar-left">
+                        <ul className="menu">
+                            <li className="menu-text">React Timer App</li>
+                            <li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
+                            <li><Link to="/" activeClassName="active-link">Countdown</Link></li>
+                        </ul>
+                    </div>
+                    <div className="top-bar-right">
+                        <ul className="menu">
+                            <li className="menu-text">Created by Tom Vinnicombe</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = Nav;
