1061033202e47a4c8e69ecf859310176c6a4f45d
Tom Vinnicombe <tom_vinnicombe@live.com.au>
2017-06-06 20:42:04 +1000

    Countdown form complete

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,9 +4,12 @@ var Nav = require('Nav');
 var Main = (props) => {
   return (
     <div>
-          <Nav/>
-          <p>Main.jsx Rendered</p>
-          {props.children}
+      <Nav/>
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
+            {props.children}
+        </div>
+      </div>
     </div>
   );
 }
